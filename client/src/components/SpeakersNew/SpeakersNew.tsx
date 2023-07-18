2372722f6aefebe938be280d08bbf41e6ed44754
Ernest Myrmyr <ernuk@MacBook-Air.local>
2023-07-18 10:20:08 +0300

    changed video. Made some changes to the buttons

--- client/src/components/SpeakersNew/SpeakersNew.tsx
+++ client/src/components/SpeakersNew/SpeakersNew.tsx
@@ -8,17 +8,21 @@ const SpeakersNew = () => {
     <div className={css.main_div}>
       <h1>Хто був спікером?</h1>
 
-      {speakersInfo.map((obj) => (
-        <Speaker speaker={obj} />
-      ))}
+      <div className={css.speakersMapping}>
+        {speakersInfo.map((obj) => (
+          <Speaker speaker={obj} />
+        ))}
+      </div>
 
       <div className={css.video_block}>
         <iframe
+          width="966"
+          height="543"
+          src="https://www.youtube.com/embed/5bVVRqzz5Wg"
           title="Intro"
-          src="https://drive.google.com/file/d/1EKj32jurLhAjJI2-kT0ItkuCX4UnYcUM/preview"
-          width="1080"
-          height="607.50"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
         ></iframe>
       </div>
     </div>
